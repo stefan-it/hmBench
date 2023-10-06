a20f0bfa5364bca87f095b6ee60d046ce7144397
Stefan Schweter <stefan@schweter.it>
2023-10-06 08:47:27 +0200

    bench: shorten model upload path (max is 96 chars...)

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ fine_tuner = importlib.import_module("flair-fine-tuner")
 
 config_file = os.environ.get("CONFIG")
 hf_token = os.environ.get("HF_TOKEN")
-repo_name = os.environ.get("REPO_NAME")
+hf_hub_org_name = os.environ.get("HUB_ORG_NAME")
 
 login(token=hf_token, add_to_git_credential=True)
 api = HfApi()
@@ -38,10 +38,26 @@ for seed in seeds:
         for epoch in epochs:
             for learning_rate in learning_rates:
                 for subword_pooling in subword_poolings:
-                    fine_tuner.run_experiment(seed, batch_size, epoch, learning_rate, subword_pooling, hipe_datasets, json_config)
+                    fine_tuner.run_experiment(seed, batch_size, epoch, learning_rate, subword_pooling, hipe_datasets,
+                                              json_config)
+                    datasets = "-".join([dataset for dataset in hipe_datasets]).replace("/", "-")
+                    hf_model = json_config["hf_model"].replace("/", "-").split("-")[0]
+                    context_size = json_config["context_size"]
+                    layers = json_config["layers"] if "layers" in json_config else "-1"
+                    use_crf = json_config["use_crf"] if "use_crf" in json_config else False
+
+                    repo_name = f"hmbench-{datasets}-{hf_model}-bs{batch_size}-ws{context_size}-e{epoch}-lr{learning_rate}-pooling{subword_pooling}-layers{layers}-crf{use_crf}-{seed}"
+
+                    repo_url = api.create_repo(
+                        repo_id=f"{hf_hub_org_name}/{repo_name}",
+                        token=hf_token,
+                        private=True,
+                        exist_ok=True,
+                    )
+
                     api.upload_folder(
                         folder_path="./",
                         path_in_repo="./",
-                        repo_id=repo_name,
-                        repo_type="dataset",
+                        repo_id=f"{hf_hub_org_name}/{repo_name}",
+                        repo_type="model",
                     )
